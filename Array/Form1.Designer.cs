27900f60b18fc3314a9dd09533507be087b3a3c0
SurawutSangraumjai <65040233126@udru.ac.th>
2024-11-25 12:03:51 +0700

    Add project files.

---
+++ Array/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace Array
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            button3 = new Button();
+            label2 = new Label();
+            label3 = new Label();
+            button4 = new Button();
+            button5 = new Button();
+            checkedListBox1 = new CheckedListBox();
+            comboBox1 = new ComboBox();
+            listBox1 = new ListBox();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(52, 38);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 0;
+            button1.Text = "Array1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(52, 92);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 1;
+            button2.Text = "Array2";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(159, 38);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(246, 327);
+            textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.BackColor = Color.FromArgb(255, 255, 192);
+            label1.BorderStyle = BorderStyle.FixedSingle;
+            label1.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(547, 38);
+            label1.Name = "label1";
+            label1.Size = new Size(219, 327);
+            label1.TabIndex = 3;
+            // 
+            // button3
+            // 
+            button3.FlatStyle = FlatStyle.Popup;
+            button3.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button3.ImageAlign = ContentAlignment.TopRight;
+            button3.Location = new Point(421, 42);
+            button3.Name = "button3";
+            button3.Size = new Size(108, 73);
+            button3.TabIndex = 4;
+            button3.Text = "GetData";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(159, 18);
+            label2.Name = "label2";
+            label2.Size = new Size(88, 20);
+            label2.TabIndex = 5;
+            label2.Text = "พิมพ์รายชื่อ";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(547, 17);
+            label3.Name = "label3";
+            label3.Size = new Size(129, 20);
+            label3.TabIndex = 6;
+            label3.Text = "รายชื่อที่บันทึกแล้ว";
+            // 
+            // button4
+            // 
+            button4.FlatStyle = FlatStyle.Popup;
+            button4.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button4.ImageAlign = ContentAlignment.TopRight;
+            button4.Location = new Point(421, 140);
+            button4.Name = "button4";
+            button4.Size = new Size(108, 73);
+            button4.TabIndex = 7;
+            button4.Text = "GetNumber";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(52, 154);
+            button5.Name = "button5";
+            button5.Size = new Size(75, 23);
+            button5.TabIndex = 8;
+            button5.Text = "Array 2 มิติ";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // checkedListBox1
+            // 
+            checkedListBox1.FormattingEnabled = true;
+            checkedListBox1.Location = new Point(818, 42);
+            checkedListBox1.Name = "checkedListBox1";
+            checkedListBox1.Size = new Size(120, 94);
+            checkedListBox1.TabIndex = 9;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(817, 155);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(121, 23);
+            comboBox1.TabIndex = 10;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(818, 203);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(120, 94);
+            listBox1.TabIndex = 11;
+            listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(991, 450);
+            Controls.Add(listBox1);
+            Controls.Add(comboBox1);
+            Controls.Add(checkedListBox1);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(button3);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private TextBox textBox1;
+        private Label label1;
+        private Button button3;
+        private Label label2;
+        private Label label3;
+        private Button button4;
+        private Button button5;
+        private CheckedListBox checkedListBox1;
+        private ComboBox comboBox1;
+        private ListBox listBox1;
+    }
+}
